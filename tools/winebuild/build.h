58e5469edd95aecb86b0658c293c05896e2a3135
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-09-24 04:36:02 -0400

    winebuild: define OS/2 and POSIX subsystem values

--- tools/winebuild/build.h
+++ tools/winebuild/build.h
@@ -223,6 +223,8 @@ static inline int is_pe(void)
 #define	IMAGE_SUBSYSTEM_NATIVE      1
 #define	IMAGE_SUBSYSTEM_WINDOWS_GUI 2
 #define	IMAGE_SUBSYSTEM_WINDOWS_CUI 3
+#define	IMAGE_SUBSYSTEM_OS2_CUI     5
+#define	IMAGE_SUBSYSTEM_POSIX_CUI   7
 #define	IMAGE_SUBSYSTEM_WINDOWS_CE_GUI 9
 
 /* global functions */
