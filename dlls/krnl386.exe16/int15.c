d1062881dbe2279464edf842aa17ec2c498fb836
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-09-26 03:43:21 -0400

    krnl386: type INT 15h state as i386

--- dlls/krnl386.exe16/int15.c
+++ dlls/krnl386.exe16/int15.c
@@ -31,7 +31,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(int);
  *
  * Handler for int 15h
  */
-void WINAPI DOSVM_Int15Handler( CONTEXT *context )
+void WINAPI DOSVM_Int15Handler( I386_CONTEXT *context )
 {
     switch(AH_reg(context))
     {
