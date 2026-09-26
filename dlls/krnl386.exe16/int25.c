291c807a4d058d23fe1d71fb75f8cd5743ae17ec
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-09-26 03:43:32 -0400

    krnl386: type INT 25h state as i386

--- dlls/krnl386.exe16/int25.c
+++ dlls/krnl386.exe16/int25.c
@@ -76,7 +76,7 @@ BOOL DOSVM_RawRead(BYTE drive, DWORD begin, DWORD nr_sect, BYTE *dataptr, BOOL f
  *
  * Handler for int 25h (absolute disk read).
  */
-void WINAPI DOSVM_Int25Handler( CONTEXT *context )
+void WINAPI DOSVM_Int25Handler( I386_CONTEXT *context )
 {
     WCHAR drivespec[] = {'A', ':', '\\', 0};
     BYTE *dataptr = ldt_get_ptr( context->SegDs, context->Ebx );
