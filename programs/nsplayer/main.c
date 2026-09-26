5c51ad6bc796888cc0dc09bcb2398c7d99c86839
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-09-26 13:06:00 -0400

    nsplayer: process DirectShow events without blocking

--- programs/nsplayer/main.c
+++ programs/nsplayer/main.c
@@ -23,12 +23,92 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(nsplayer);
 
+static HRESULT wait_for_completion(IMediaEvent *event)
+{
+    HANDLE event_handle;
+    HRESULT hr = S_OK;
+    BOOL done = FALSE;
+
+    hr = IMediaEvent_GetEventHandle(event, (OAEVENT *)&event_handle);
+    if (FAILED(hr))
+        return hr;
+
+    while (!done)
+    {
+        DWORD wait = MsgWaitForMultipleObjects(1, &event_handle, FALSE, INFINITE, QS_ALLINPUT);
+
+        if (wait == WAIT_OBJECT_0)
+        {
+            LONG event_code;
+            LONG_PTR param1, param2;
+
+            while (IMediaEvent_GetEvent(event, &event_code, &param1, &param2, 0) == S_OK)
+            {
+                HRESULT event_hr = S_OK;
+                BOOL terminal = FALSE;
+
+                TRACE("media event %#lx\n", event_code);
+
+                switch (event_code)
+                {
+                case EC_COMPLETE:
+                    terminal = TRUE;
+                    break;
+
+                case EC_USERABORT:
+                    terminal = TRUE;
+                    break;
+
+                case EC_ERRORABORT:
+                    event_hr = (HRESULT)param1;
+                    if (SUCCEEDED(event_hr))
+                        event_hr = E_FAIL;
+                    terminal = TRUE;
+                    break;
+                }
+
+                IMediaEvent_FreeEventParams(event, event_code, param1, param2);
+
+                if (terminal)
+                {
+                    hr = event_hr;
+                    done = TRUE;
+                    break;
+                }
+            }
+        }
+        else if (wait == WAIT_OBJECT_0 + 1)
+        {
+            MSG message;
+
+            while (PeekMessageW(&message, NULL, 0, 0, PM_REMOVE))
+            {
+                if (message.message == WM_QUIT)
+                {
+                    hr = E_ABORT;
+                    done = TRUE;
+                    break;
+                }
+
+                TranslateMessage(&message);
+                DispatchMessageW(&message);
+            }
+        }
+        else
+        {
+            hr = wait == WAIT_FAILED ? HRESULT_FROM_WIN32(GetLastError()) : E_UNEXPECTED;
+            break;
+        }
+    }
+
+    return hr;
+}
+
 static HRESULT play_target(const WCHAR *target)
 {
     IMediaControl *control = NULL;
     IMediaEvent *event = NULL;
     BSTR filename;
-    LONG event_code = 0;
     HRESULT hr;
 
     hr = CoCreateInstance(&CLSID_FilterGraph, NULL, CLSCTX_INPROC_SERVER,
@@ -63,13 +143,13 @@ static HRESULT play_target(const WCHAR *target)
 
     hr = IMediaControl_Run(control);
     if (SUCCEEDED(hr))
-        hr = IMediaEvent_WaitForCompletion(event, INFINITE, &event_code);
+        hr = wait_for_completion(event);
 
     IMediaControl_Stop(control);
     IMediaEvent_Release(event);
     IMediaControl_Release(control);
 
-    TRACE("playback ended with event %ld, hr %#lx\n", event_code, hr);
+    TRACE("playback ended, hr %#lx\n", hr);
     return hr;
 }
 
