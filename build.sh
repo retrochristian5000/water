1a77300166853cf9848b2d1e63c5b9eb89d2320f
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-09-26 13:26:43 -0400

    build: bump profile schema for linker policy

--- build.sh
+++ build.sh
@@ -1295,7 +1295,7 @@ setup_toolchain()
 profile_signature()
 {
     printf '%s\n' \
-        "WHP_PROFILE_SCHEMA=3" \
+        "WHP_PROFILE_SCHEMA=4" \
         "WATER_ARCHS_MODE=${WATER_ARCHS_MODE:-auto}" \
         "WATER_LLVM_BOOTSTRAP=${WATER_LLVM_BOOTSTRAP:-auto}" \
         "WATER_LLVM_BUILD_TYPE=${WATER_LLVM_BUILD_TYPE:-Release}" \
